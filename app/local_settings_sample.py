8235d82e9e28bbc40a9c0aba622dcd92caec9a65
mrt <matt.toronto97@gmail.com>
2025-02-23 15:44:16 -0800

    Updates

---
+++ app/local_settings_sample.py
@@ -0,0 +1,22 @@
+firestore_creds = {
+	"type": "service_account",
+	"project_id": "...",
+	"private_key_id": "...",
+	"private_key": "...",
+	"client_email": "...",
+	"client_id": "...", 
+	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+	"token_uri": "https://oauth2.googleapis.com/token",
+	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+	"client_x509_cert_url": "..."
+}
+
+OPENAI_API_KEY_GPT4 = "..."
+
+# Zoho Mail API Credentials
+# Required scopes: ZohoMail.messages.ALL,ZohoMail.accounts.READ
+ZOHO_MAIL_CLIENT_ID = "..."
+ZOHO_MAIL_CLIENT_SECRET = "..."
+ZOHO_MAIL_REFRESH_TOKEN = "..."
+
+
